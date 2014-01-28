fb985cab9d2385894c42831750863508c3054e80
Mark Lassoff <mark@learntoprogram.tv>
2014-01-27 23:13:40 -0500

    Updated View

--- nprClient.js
+++ nprClient.js
@@ -14,6 +14,7 @@ if (xmlhttp.readyState==4 && xmlhttp.status==200)
 {
 	var ids = new Array();
 	var titles = new Array();
+	var info = new Array();
 	var output ="<ul>";
 	//Parse XML in to two parallel arrays
 	var theXML = xmlhttp.responseXML.documentElement.getElementsByTagName('item');
@@ -22,16 +23,20 @@ if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		{
 			ids[i] = theXML[i].getAttribute('id');
 			titles[i]= theXML[i].getElementsByTagName('title')[0].firstChild.textContent;
-			output += buildOutputCell(ids[i], titles[i]);
+			info[i]= theXML[i].getElementsByTagName('additionalInfo')[0].firstChild.textContent;
+			output += buildOutputCell(ids[i], titles[i], info[i]);
 		}
 	output += "</ul>";
 	document.getElementById('result').innerHTML = output;
 }
 }
 
-function buildOutputCell(id, data)
+function buildOutputCell(id, data, info)
 {
-cellHTML = "<li onclick='getNews(" + id + ")'>" + data + "</li>";
+cellHTML = "<li onclick='getNews(" + id + ")'>"
+cellHTML += "<h3>" + data + "</h3>";
+cellHTML += "<p>" + info + "</p>"; 
+cellHTML += "</li>";
 return cellHTML;
 }
 
