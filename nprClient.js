ffecef2a86f99e3fb6d4aaf97584024d67b6fae2
Mark Lassoff <mark@learntoprogram.tv>
2014-01-28 10:25:07 -0500

    Pulling stories from NPR feed

--- nprClient.js
+++ nprClient.js
@@ -33,14 +33,46 @@ if (xmlhttp.readyState==4 && xmlhttp.status==200)
 
 function buildOutputCell(id, data, info)
 {
-cellHTML = "<li onclick='getNews(" + id + ")'>"
-cellHTML += "<h3>" + data + "</h3>";
-cellHTML += "<p>" + info + "</p>"; 
-cellHTML += "</li>";
-return cellHTML;
+    cellHTML = "<li onclick='getNews(" + id + ")'>"
+    cellHTML += "<h3>" + data + "</h3>";
+    cellHTML += "<p>" + info + "</p>"; 
+    cellHTML += "</li>";
+    return cellHTML;
 }
 
 function getNews(id)
 {
-	
+    var url = "http://api.npr.org/query?id=" + id + "&output=RSS&apiKey=MDEzMDc4Mjg4MDEzOTA4NjIzNTZkYzEwYw001";
+    console.log(url);
+    xmlhttp.onreadystatechange = processNewStory;
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+    
+}
+
+function processNewStory()
+{
+    if(xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+        var output = "";
+        var storyXML = xmlhttp.responseXML.documentElement;
+        console.log(storyXML);
+        output += "<p><em>" + storyXML.getElementsByTagName('copyright')[0].firstChild.nodeValue + "</em></p>";
+        var stories =  storyXML.getElementsByTagName('item');
+        var numStories = stories.length;
+        for(var i=0; i < numStories; i++)
+        {
+            var title = stories[i].getElementsByTagName('title')[0].firstChild.nodeValue;
+            var description = stories[i].getElementsByTagName('description')[0].firstChild.nodeValue;
+            var pubDate = stories[i].getElementsByTagName('pubDate')[0].firstChild.nodeValue;
+           // var contentEncoded = stories[0].getElementsByTagName('contentEncoded')[i].firstChild.nodeValue;
+            var storyOutput = "<p>" + title + "</p>";
+            storyOutput += "<p>" + description + "</p>";
+            storyOutput += "<p>" + pubDate + "</p>";
+            output += storyOutput;
+            
+        }
+        document.getElementById("result").innerHTML = output;
+        
+    }
 }
